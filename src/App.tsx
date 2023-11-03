63dfbf486f0137dd92af741445f476757142c0b2
lucas <lucasriibeiro23@gmail.com>
2023-11-03 19:22:29 -0300

    change files

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import { Route, Routes } from 'react-router-dom';
-import Initial from './pages/Initial';
-import Counter from './pages/Counter';
+import Home from './pages/Home';
 import './App.css';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={ <Initial /> } />
-      <Route path="/counter" element={ <Counter /> } />
+      <Route path="/" element={ <Home /> } />
     </Routes>
   );
 }
