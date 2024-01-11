4a1b3c2ac70a5740b7b82f212a8c38ef37cba81d
lucasribeiro97 <lucasriibeiro23@gmail.com>
2024-01-11 17:13:13 -0300

    implementei icones de linkedin, github e whatsapp no header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,7 +4,7 @@ function Header() {
       <div>
         <h1 className="text-2xl font-semibold text-center text-white">Lucas Ribeiro</h1>
         <img
-          src="public/profile-picture.jpeg"
+          src="profile-picture.jpeg"
           alt="Foto de perfil"
           className="profile-img"
         />
@@ -13,6 +13,23 @@ function Header() {
           Seja bem vindo(a) ao meu site pessoal!
         </p>
       </div>
+      <ul className="flex flex-row">
+        <li>
+          <a href="https://www.linkedin.com/in/lucasribeirocunha/" target="_blank" rel="noreferrer">
+            <img src="icons/linkedin.svg" alt="logo do linkedin" />
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/lucasribeiro97" target="_blank" rel="noreferrer">
+            <img src="icons/github.svg" alt="logo do github" />
+          </a>
+        </li>
+        <li>
+          <a href="https://api.whatsapp.com/send?phone=5518996807371">
+            <img src="icons/whatsapp.svg" alt="logo do whatsapp" />
+          </a>
+        </li>
+      </ul>
     </header>
   );
 }
