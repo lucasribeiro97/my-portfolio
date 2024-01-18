b5e8a8c7e82aab9e095a5176b1d03aa60f8eba20
lucasribeiro97 <lucasriibeiro23@gmail.com>
2024-01-18 19:10:41 -0300

    comecei a adicionar os icones na lista de skills

--- src/components/SkillsList/SkillsList.tsx
+++ src/components/SkillsList/SkillsList.tsx
@@ -1,15 +1,33 @@
 function SkillsList() {
   return (
     <div className="flex flex-col px-56 py-5">
-      <h2 className="text-2xl font-semibold py-1">O que eu faço?</h2>
-      <p className="text-md font-light py-1 pb-3">
-        Sou desenvolvedor Full Stack pronto para novos desafios.
-        Abaixo está uma visão de geral dos meus principais conjuntos
-        de habilidades técnicas e tecnologias que utilizo.
-        Acesse meu currículo online e portfólio de projetos para mais detalhes.
-      </p>
-      <ul>
-        <li />
+      <div className="flex flex-col mb-10">
+        <h2 className="text-2xl font-semibold py-1">O que eu faço?</h2>
+        <p className="text-md font-light py-1 pb-3">
+          Sou desenvolvedor Full Stack pronto para novos desafios.
+          Abaixo está uma visão de geral dos meus principais conjuntos
+          de habilidades técnicas e tecnologias que utilizo.
+          Acesse meu currículo online e portfólio de projetos para mais detalhes.
+        </p>
+      </div>
+      <ul className="flex">
+        <li className="space-y-1">
+          <img src="icons-technologies/react.svg" alt="ícone do React" />
+          <h2 className="text-xl font-semibold">React</h2>
+          <p>
+            Testes automatizados, Componentes de classes e funcionais,
+            estados, eventos e estilização de componentes, React Router, Redux,
+            Context API e Hooks.
+          </p>
+        </li>
+        <li>
+          <img src="icons-technologies/node.svg" alt="ícone do Node.js" />
+          <h2 className="text-xl font-semibold">Node.js</h2>
+          <p>
+            Express, Sequelize, Knex, JWT, testes automatizados,
+            RESTful APIs, GraphQL, WebSockets e Socket.io.
+          </p>
+        </li>
       </ul>
     </div>
   );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,7 @@ import SkillsList from '../components/SkillsList/SkillsList';
 
 function Home() {
   return (
-    <div className="bg-blue-600 h-screen">
+    <div className="h-screen">
       <About />
       <SkillsList />
     </div>
